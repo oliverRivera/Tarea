2f78d86a2733bd61fc798c2b046815be6eab184d
Oliver Rivera <alxrive@gmail.com>
2020-02-09 15:10:13 -0600

    "Subiendo tarea Actividad 2"

---
+++ Actividad2.js
@@ -0,0 +1,13 @@
+var mes = 10;
+if (mes == 12 || mes == 1 || mes == 2) {
+  document.write ("Es invierno<br");
+} else if (mes >= 3 && mes <= 5){
+  document.write ("Es primavera<br>");
+} else if (mes >= 6 && mes <= 8){
+  document.write ("Es verano<br>");
+} else if (mes >=9 && mes <= 11){
+  document.write ("Es otoÃ±o<br>");
+} else {
+  alert ("Mes no existe");
+}
+document.write ("<br>10111164" + "<br> Oliver Alexander Rivera Vela");
